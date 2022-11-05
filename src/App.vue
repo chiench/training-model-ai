d724794bf849028f5bb17e32276a00c6239bdf20
chiench <quangminhb53@gmail.com>
2022-11-06 06:51:56 +0700

    Add into App main

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@
     <v-main>
       <HelloWorld />
     </v-main>
+    <FooterComponet />
   </v-app>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
 import HeaderComponet from "./components/header/HeaderComponent.vue";
+import FooterComponet from "./components/footer/FooterComponent.vue";
 
 export default {
   name: "App",
@@ -18,6 +20,8 @@ export default {
     HelloWorld,
     // eslint-disable-next-line vue/no-unused-components
     HeaderComponet,
+    // eslint-disable-next-line vue/no-unused-components
+    FooterComponet,
   },
 
   data: () => ({
