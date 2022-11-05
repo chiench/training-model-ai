7e573422d93fa469d1c923fed1347cb85b26ebcf
chiench <quangminhb53@gmail.com>
2022-11-05 23:08:21 +0700

    Change headercomponent

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <HeaderComponet />
     <v-main>
       <HelloWorld />
     </v-main>
--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -1,24 +1,19 @@
 <template>
-  <v-app-bar
-    elevation="7"
-    app
-    light
-    color="white"
-    elevate-on-scroll
-    style="padding: 0 100px"
-  >
-    <v-toolbar-title>Web Vuetify</v-toolbar-title>
-    <v-spacer />
-    <v-list>
-      <v-list-item-group class="d-flex justify-space-around">
-        <v-list-item v-for="(item, i) in items" :key="i">
-          <v-list-item-content>
-            {{ item.tilte }}
-          </v-list-item-content>
-        </v-list-item>
-        <v-btn outlined elevation="2"><v-icon>mdi-magnify</v-icon></v-btn>
-      </v-list-item-group>
-    </v-list>
+  <v-app-bar app color="#424242" dark>
+    <div style="width: 100%" class="d-flex justify-center align-center">
+      <v-img
+        alt="AI Logo"
+        class="shrink mr-2 rounded-circle"
+        contain
+        src="../../assets/openai-avatar.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <span class="h6 text-uppercase font-weight-medium mr-2"
+        >Predic Netword</span
+      >
+    </div>
   </v-app-bar>
 </template>
 
